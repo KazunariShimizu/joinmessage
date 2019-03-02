0f4755b4a92b12d14f517212d0af9cf6ceaad90a
BuildTools <unconfigured@null.spigotmc.org>
2019-03-02 22:32:43 +0900

    最初のコミット

---
+++ src/jp/mydns/toraden/joinmessage/Main.java
@@ -0,0 +1,16 @@
+package jp.mydns.toraden.joinmessage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		System.out.println("プラグインが有効になりました");
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("プラグインが無効になりました");
+	}
+}
