4f40cc35a6755cab40d264b9d6ea5ef60bbb4647
BuildTools <unconfigured@null.spigotmc.org>
2019-03-03 00:36:07 +0900

    完成

--- src/jp/mydns/toraden/joinmessage/Main.java
+++ src/jp/mydns/toraden/joinmessage/Main.java
@@ -1,16 +1,33 @@
 package jp.mydns.toraden.joinmessage;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+
+public class Main extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		
 		System.out.println("プラグインが有効になりました");
 	}
 	
 	@Override
 	public void onDisable() {
 		System.out.println("プラグインが無効になりました");
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player p = e.getPlayer();
+		if(p.hasPlayedBefore()) {
+			e.setJoinMessage("プレイヤーがログインしました: " + e.getPlayer().getName());
+		}else {
+			e.setJoinMessage("プレイヤーが初めてログインしました: " + e.getPlayer().getName());
+		}
 	}
 }
